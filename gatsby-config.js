c2db4b829433154f10cd2b558e11f0be83ca0497
Mark Fang <fangxuhe@gmail.com>
2020-10-25 12:51:56 -0400

    Watch text and title change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mark's Crazy Website`,
+    description: `What? This is just crazy!`,
+    author: `@MaaaaarkF`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
